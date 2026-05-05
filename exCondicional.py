c2c9d1a4d426a0b38fda50cf8e6e85db0f481e52
GustavoWachholz <gustavoawm13@live.com>
2026-05-04 21:31:50 -0300

    Código do desconto

---
+++ exCondicional.py
@@ -0,0 +1,14 @@
+valor = float(input("Digite o valor do pedido: "))
+
+if valor <= 100:
+    desconto = 0.95
+
+elif valor > 100 and valor < 300:
+    desconto = 0.90
+
+else:
+    desconto = 0.85
+    
+total = valor * desconto
+percentualDesconto = (1 - desconto) * 100
+print(f"O valor total da compra foi de R$ {round(valor, 2)}. \nValor total foi de: R$ {round(total, 2)}. \nO deconto foi de: {round(percentualDesconto)}%")
