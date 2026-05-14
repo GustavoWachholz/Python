2cd9c47cdd48345cd39b42aeaf4a920076f6fc37
GustavoWachholz <gustavoawm13@live.com>
2026-05-13 21:56:43 -0300

    Atividade while 2

---
+++ ExercicioWhile2.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número. Digite 0 para parar: "))
+cont = 0
+
+while numero != 0:
+    cont += 1
+    numero = int(input("Digite um número. Digite 0 para parar: "))
+
+print(cont)
